1ca7bb7dc7b756d978079fed2f5bf9a67d46419f
razercreedunity <iwanroslan35@gmail.com>
2023-06-11 12:39:51 +0800

    first commit

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+
+  private apiUrl = 'http://localhost:8080';
+  
+  constructor(private _http: HttpClient) {}
+
+  addEmployee(data: any): Observable<any> {
+    return this._http.post(`${this.apiUrl}/post`, data);
+  }
+
+  updateEmployee(id: number, data: any): Observable<any> {
+    return this._http.patch(`${this.apiUrl}/update/${id}`, data);
+  }
+
+  getEmployeeList(): Observable<any> {
+    return this._http.get(`${this.apiUrl}/getAll`);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this._http.delete(`${this.apiUrl}/delete/${id}`);
+  }
+}
